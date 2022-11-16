af01f4f98d974fbf4cd4a2de0d4d7dec23c4df2a
Марк Міллер <youtte1g@gmail.com>
2022-11-16 21:59:22 +0200

    start developing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] matrix = {{0, 1, 2}, {4, 5, 6}, {1, 1, 1}};
+        int[] result = getDiagonal(matrix);
+        System.out.println(Arrays.toString(result));
+        /*
+         0 1 2
+         4 5 6
+         1 1 1
+         */
+
+    }
+
+    public static int[] getDiagonal(int[][] matrix) {
+        int returned[] = new int[matrix.length];
+        int count = 0;
+        for (int i = 0; i < returned.length; i++) {
+            for (int j = 0; i < matrix[i].length; i++) {
+                returned[i] += matrix[count][i];
+                count++;
+                break;
+            }
+
+        }
+        return returned;
+
     }
 }
